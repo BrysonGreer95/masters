5b8a1e6cefc83c9d982be3a47e3372c106b7e3b9
Bryson Greer <bryson.greer2014@gmail.com>
2024-04-01 18:05:23 -0400

    typo

--- src/views/Scramble.vue
+++ src/views/Scramble.vue
@@ -12,7 +12,7 @@
                     { 'team': 2, 'member_1': 'Andy Jones', 'member_2': 'Blake Jones', "tee_time": "10:04"},
                     { 'team': 3, 'member_1': 'Brad Barbour', 'member_2': 'Lee Chatham', "tee_time": "10:12"},
                     { 'team': 4, 'member_1': 'Markell Quinn', 'member_2': 'Wyatt Dershem', "tee_time": "10:12"},
-                    { 'team': 5, 'member_1': 'Erin Leon', 'member_2': 'Brandon Massie', "tee_time": "10:20"},
+                    { 'team': 5, 'member_1': 'Eric Leon', 'member_2': 'Brandon Massie', "tee_time": "10:20"},
                     { 'team': 6, 'member_1': 'N/A', 'member_2': 'N/A', "tee_time": "10:20"},
 
                 ],
