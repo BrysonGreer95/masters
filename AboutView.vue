cbe0d069133ce9beea762cc0cbae1b754bb31f67
Bryson Greer <52019601+BrysonGreer95@users.noreply.github.com>
2026-04-05 17:07:59 -0400

    Update point distribution for Mini Golf and Scramble

---
+++ AboutView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Point Distribution</h1>
+    <h2>Mini Golf</h2>
+    <ul>
+      <li>1st place: 300 points</li>
+      <li>2nd place: 250 points</li>
+      <li>3rd place: 200 points</li>
+      <li>4th place: 150 points</li>
+      <li>5th place: 100 points</li>
+      <li>6th place: 75 points</li>
+      <li>7th place: 50 points</li>
+      <li>8th place: 25 points</li>
+      <li>9th place: 10 points</li>
+      <li>10th place: 5 points</li>
+      <li>11th place: 0 points</li>
+      <li>12th place: 0 points</li>
+      <li>13th place: 0 points</li>
+      <li>14th place: 0 points</li>
+    </ul>
+
+    <h2>Scramble</h2>
+    <ul>
+      <li>1st place: 200 points each</li>
+      <li>2nd place: 175 points each</li>
+      <li>3rd place: 150 points each</li>
+      <li>4th place: 125 points each</li>
+      <li>5th place: 100 points each</li>
+      <li>6th place: 75 points each</li>
+      <li>7th place: 50 points each</li>
+    </ul>
+  </div>
+</template>
